3bec797ccac55f7867e3aa06bb1b23f067109aac
lunablanco <mcecilb@gmail.com>
2025-03-17 09:13:02 -0300

    Variable y funciones para el reto de amigo secreto

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variables
+const inputAmigo = document.getElementById('amigo');
+const listaAmigos = [];
+const amigoSorteado = document.getElementById('amigo-sorteado');
+
+//Funciones
+function agregarAmigo() {
+    if (inputAmigo.value === '') {
+        alert('Debes ingresar un nombre');
+    } else {
+        listaAmigos.push(inputAmigo.value);
+        inputAmigo.value = '';
+        console.log(listaAmigos);
+    }
+}
+agregarAmigo();
+
+function sortearAmigo() {
+    const amigoSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+    console.log(amigoSorteado);
+    alert(`Tu amigo secreto es: ${amigoSorteado}`);
+    }
